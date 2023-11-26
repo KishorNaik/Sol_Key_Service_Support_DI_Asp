4f4868603384f9e142cf361c0356aeeee886b006
Kishor Naik <kishor.naik011.net@gmail.com>
2023-11-26 10:39:39 +0530

    Code Commit : Added

---
+++ Sol_Demo/Sol_Demo/Controllers/DemoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Demo.Core;
+
+namespace Sol_Demo.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet("get/small")]
+        public IActionResult GetSmallCache([FromKeyedServices(CacheDemoEnum.Small)] ICacheDemo cacheDemo)
+        {
+            return Ok(cacheDemo.Get("Hello-small"));
+        }
+
+        [HttpGet("get/big")]
+        public IActionResult GetBigCache([FromKeyedServices(CacheDemoEnum.Big)] ICacheDemo cacheDemo)
+        {
+            return Ok(cacheDemo.Get("Hello-big"));
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Core/CacheDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace Sol_Demo.Core;
+
+public enum CacheDemoEnum
+{
+    Big,
+    Small
+}
+
+public interface ICacheDemo
+{
+    object Get(string key);
+}
+
+public class BigCache : ICacheDemo
+{
+    public object Get(string key) => $"Resolving {key} from big cache.";
+}
+
+public class SmallCache : ICacheDemo
+{
+    public object Get(string key) => $"Resolving {key} from small cache.";
+}
